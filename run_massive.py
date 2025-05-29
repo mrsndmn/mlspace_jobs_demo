cba4ab5934e444a68fbc409c7a2e2fb29ece8868
Dmitrii Tarasov <d.tarasov@airi.net>
2025-05-29 19:13:46 +0300

    recoding finished

--- run_massive.py
+++ run_massive.py
@@ -9,12 +9,14 @@ if __name__ == "__main__":
 
     workdir = os.getcwd()
 
+    author_name = 'd.tarasov'
+
     for learning_rate in [ 0.1, 0.01 ]:
         for batch_size in [ 16, 32 ]:
             result = client.run_job(
                 payload={
                     'script': f"bash {workdir}/scripts/run_training_multigpu_accelerate.sh --learning_rate {learning_rate} --per_device_train_batch_size {batch_size} --output_dir ./train_demo_lr_{learning_rate}_bs_{batch_size}",
-                    'job_desc': f'Massive Jobs Demo {learning_rate} {batch_size} #author_name #rnd #multimodal',
+                    'job_desc': f'Massive Jobs Demo {learning_rate} {batch_size} #{author_name} #rnd #multimodal',
                     'env_variables': {
                         'WANDB_API_KEY': os.environ.get('WANDB_API_KEY'),
                     },
@@ -23,8 +25,8 @@ if __name__ == "__main__":
                     'type': 'binary_exp',
                     'shm_size_class': 'medium',
                     'base_image': 'cr.ai.cloud.ru/aicloud-base-images/cuda12.1-torch2-py311:0.0.36',
-                    'n_workers': 1,              # Не трогайте эти параметры! Они ничего не ускорят, а только скорее могут сломать Multi-GPU обучение. Изменяйте их только если вы точно понимаете, что делаете
-                    'processes_per_worker': 1,   # Не трогайте эти параметры! Они ничего не ускорят, а только скорее могут сломать Multi-GPU обучение. Изменяйте их только если вы точно понимаете, что делаете
+                    'n_workers': 1,              # Количество воркеров.
+                    'processes_per_worker': 1,   # Количество процессов на воркер. Для accelerate нужно запускать 1 процесс на воркер. Для torchrun лучше не заполнять этот параметр. По умолчанию запускается по количеству GPU на одном воркере - это подходит для torchrun.
                 }
             )
 
