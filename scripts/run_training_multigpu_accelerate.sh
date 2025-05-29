35edff9842bd9f600464444f45d1db6d8bb38f0a
Dmitrii Tarasov <d.tarasov@airi.net>
2025-05-29 16:56:54 +0300

    any fixes

--- scripts/run_training_multigpu_accelerate.sh
+++ scripts/run_training_multigpu_accelerate.sh
@@ -4,4 +4,4 @@ ENV_PREFIX=/workspace-SR004.nfs2/d.tarasov/envs/jobs_demo/bin
 WORKDIR=/workspace-SR004.nfs2/d.tarasov/mlspace_jobs_demo
 
 cd $WORKDIR
-$ENV_PREFIX/python $ENV_PREFIX/accelerate launch --config_file $WORKDIR/accelerate/accelerator_config_2gpu.yaml scripts/train_demo.py $@
+$ENV_PREFIX/python $ENV_PREFIX/accelerate launch --config_file $WORKDIR/accelerate/accelerator_config_2gpu.yaml scripts/train_demo_multigpu_accelerate.py $@
--- scripts/run_training_multinode.sh
+++ scripts/run_training_multinode.sh
@@ -5,16 +5,23 @@ set -ex
 ENV_PREFIX=/workspace-SR004.nfs2/d.tarasov/envs/jobs_demo/bin
 WORKDIR=/workspace-SR004.nfs2/d.tarasov/mlspace_jobs_demo
 
+# Джобы запускаются под управлением MPI
+# Поэтому нам нужно получить имя мастер-ноды
+# Чтобы сохранить его в переменные окружения,
+# с которыми работает DDP
 MASTER_HOST_PREFIX=$(perl -E "my \$x = '$PMIX_HOSTNAME'; \$x =~ s/-\w+-\d+$//; print \$x ")
 MASTER_HOST=$(perl -E "my \$x = '$PMIX_HOSTNAME'; \$x =~ s/-\w+-\d+$/-mpimaster-0/; print \$x ")
 
 MASTER_HOST_FULL="$MASTER_HOST.$MASTER_HOST_PREFIX"
 echo "MASTER_HOST_FULL $MASTER_HOST_FULL"
 
+# Сохраняем в переменные окружения,
+# с которыми работает DDP
 export MASTER_ADDR=$MASTER_HOST_FULL
 export MASTER_PORT=12345
 export WORLD_SIZE=$OMPI_COMM_WORLD_SIZE
 export RANK=$OMPI_COMM_WORLD_RANK
 
+# Запускаем обучение
 cd $WORKDIR
-$ENV_PREFIX/python scripts/train_demo.py $@
+$ENV_PREFIX/python scripts/train_demo_multinode_torchddp.py $@
