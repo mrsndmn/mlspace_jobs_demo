e859fb290b13a8c19d72d4dfef09bad64f1ae248
Dmitrii Tarasov <15786586+mrsndmn@users.noreply.github.com>
2025-06-03 12:24:06 +0300

    Update run_training_multigpu_accelerate.sh

--- scripts/run_training_multigpu_accelerate.sh
+++ scripts/run_training_multigpu_accelerate.sh
@@ -3,5 +3,8 @@ set -ex
 ENV_PREFIX=/workspace-SR004.nfs2/d.tarasov/envs/jobs_demo/bin
 WORKDIR=/workspace-SR004.nfs2/d.tarasov/mlspace_jobs_demo
 
+# Добавим в PATH префикс окружения
+PATH=$ENV_PREFIX:$PATH
+
 cd $WORKDIR
-$ENV_PREFIX/python $ENV_PREFIX/accelerate launch --config_file $WORKDIR/accelerate/accelerator_config_2gpu.yaml scripts/train_demo_multigpu_accelerate.py $@
+accelerate launch --config_file $WORKDIR/accelerate/accelerator_config_2gpu.yaml scripts/train_demo_multigpu_accelerate.py $@
